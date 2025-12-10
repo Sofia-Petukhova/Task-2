357305a78ebc12aa5c03d187d55f3dbeecc7c92a
Sofia-Petukhova <sofiapetukhova7@gmail.com>
2025-12-11 02:21:41 +0700

    Added main function with program header and loop

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import sys
+
+def main() -> None:
+    print("Программа по вариантам 1.7, 2.7, 3.7\n")
+    while True:
+        choice = input("Выберите пункт меню: ")
+        if choice == "0":
+            break
+
+if __name__ == "__main__":
+    main()
